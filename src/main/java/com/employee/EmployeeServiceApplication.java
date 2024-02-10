c26d59ff282aa4b932309ee526c3f1969e48f29f
Rutuja Adsul <rutujaadsul15@gmail.com>
2024-02-10 17:36:35 +0530

    giving space

--- src/main/java/com/employee/EmployeeServiceApplication.java
+++ src/main/java/com/employee/EmployeeServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
 
